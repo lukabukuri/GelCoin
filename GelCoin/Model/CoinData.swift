e752e74fb934f4b7dd41968a8eea5494f3d56e33
Luka Bukuri <lukabukuri@gmail.com>
2021-08-28 17:46:22 +0400

    First commit

---
+++ GelCoin/Model/CoinData.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+struct CoinData: Decodable {
+    let conversion_rate: Double
+}
---
+++ GelCoin/Model/CoinManager.swift
@@ -0,0 +1,57 @@
+import Foundation
+
+protocol CoinManagerDelegate {
+    func didUpdatePrice(price: String, currency: String)
+    func didFailWithError(error: Error)
+}
+
+struct CoinManager {
+    
+    var delegate: CoinManagerDelegate?
+    
+    let baseURL = "https://v6.exchangerate-api.com/v6"
+    let apiKey = "fdf9a8c475ccc9def336d38d"
+
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    func getCoinPrice(for currency: String) {
+        
+        let urlString = "\(baseURL)/\(apiKey)/pair/\(currency)/GEL"
+        print(urlString)
+        
+        if let url = URL(string: urlString) {
+            
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    if let bitcoinPrice = self.parseJSON(safeData) {
+                        let priceString = String(format: "%.2f", bitcoinPrice)
+                        self.delegate?.didUpdatePrice(price: priceString, currency: currency)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ data: Data) -> Double? {
+        
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            let lastPrice = decodedData.conversion_rate
+            print(lastPrice)
+            return lastPrice
+            
+        } catch {
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
+    
+}
